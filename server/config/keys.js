cebf9c6188a24ec2925073af97830b5a72fce76d
Sourav-cpu-crypto <souravmukherjeemca1@gmail.com>
2022-12-20 17:00:47 +0530

    commit

--- server/config/keys.js
+++
@@ -1,7 +0,0 @@
-// import   {JWT_SECRET, MOGOURI} from './prod.js';
-import   {JWT_SECRET, MOGOURI}  from './dev.js';
-// if(process.env.NODE_ENV=='production'){
-    export default {JWT_SECRET, MOGOURI} 
-//  }else{
-//      module.exports = require('./dev')
-//  }
