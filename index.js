0df9e79b069a9e5f9f002c5080a88f68a321d991
Sourav-cpu-crypto <66194084+Sourav-cpu-crypto@users.noreply.github.com>
2022-12-21 23:19:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const connect=()=>{
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.log("error");
     process.exit(1);
   }
 }
